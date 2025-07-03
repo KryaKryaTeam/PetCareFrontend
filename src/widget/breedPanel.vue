fe37868cc911d0c0961c94262d238b83781627c9
AANDDARK <andriyworkkovalenko@gmail.com>
2025-07-03 18:59:49 +0300

    have done breedPanel functions, latest style and testing

--- src/widget/breedPanel.vue
+++ src/widget/breedPanel.vue
@@ -1,31 +1,44 @@
 <template class="border-2 m-12">
-    <input v-model="search" class="input" placeholder="Search breeds" /> 
-    <Suspense>
-      <template #fallback>
-        <p>sorry, need load data please wait</p>
-      </template>
-      <template #default>
-      <div class="flex flex-col w-40">
-        <button v-for="breed in dataRender" :key="breed" class="btn min-w-3xs" >
-        {{ breed }}
-        </button>
-    </div>
-  </template>
+  <input v-model="search" class="input" placeholder="Search breeds" /> 
+
+  <Suspense>
+    <template #fallback>
+      <p>sorry, need load data please wait</p>
+    </template>
+
+    <template #default>
+      <div>
+        <div class="flex flex-col w-40">
+          <button 
+            v-for="(breed, index) in dataRender" 
+            :key="breed" 
+            class="btn min-w-3xs"
+            @click="setBreed(index)"
+          >
+            {{ breed }}
+          </button>
+        </div>
+        <button @click="sendBreed()" class="btn" >save</button>
+      </div>
+    </template>
   </Suspense>
-  </template>
-  
+</template>
+
   <script setup lang="ts">
   import { ref, onMounted, watch } from 'vue';
   import { Suspense } from 'vue';
+  // data
  const props = defineProps({
   animal: String
  })
+  const emit = defineEmits(['breedFromPanel'])
   const search = ref('');
   const dataRender = ref<string[]>([]);
   const data = ref<string[]>([]);
+  let breedSaved = ''
+  // functions
   async function fetchBreeds() {
     try {
-      console.log(props.animal)
       const res = await fetch(`http://localhost:3000/breeds/${props.animal}`);
       if (!res.ok) {
         throw new Error(`Fetch failed: ${res.status}`);
@@ -39,6 +52,12 @@
     }
   }
 
+  const sendBreed = () => {
+    emit('breedFromPanel', breedSaved)
+  }
+
+  const setBreed = (index: number) => breedSaved = dataRender.value[index]  
+  //  mounts
   onMounted(fetchBreeds);
 
   // Filter logic
