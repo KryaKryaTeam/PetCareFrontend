965774863682fa404769ba5ed85de1a726193da5
Ivan <ivan.hryshchuk2011@gmail.com>
2025-07-19 15:09:29 +0300

    hot fix

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,24 +1,24 @@
 import Board from "@/pages/board.vue"
 import Create from "@/pages/create.vue"
 import LandingPage from "@/pages/landing/landingPage.vue"
+import PrivacyPage from "@/pages/landing/privacyPage.vue"
+import TermsPage from "@/pages/landing/termsPage.vue"
 import SingUp from "@/pages/auth/singUp.vue"
 import { createRouter, createWebHistory } from "vue-router"
 import SingIn from "@/pages/auth/singIn.vue"
 import Test from "@/pages/test.vue"
-import PrivacyPage from "@/pages/landing/privacyPage.vue"
-import TermsPage from "@/pages/landing/termspage.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
-    if (to.hash) {
-      return { el: to.hash, behavior: 'smooth' }
-    } else if (savedPosition) {
-      return savedPosition
-    } else {
-      return { left: 0, top: 0 }
-    }
-  },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return { el: to.hash, behavior: "smooth" }
+        } else if (savedPosition) {
+            return savedPosition
+        } else {
+            return { left: 0, top: 0 }
+        }
+    },
     routes: [
         {
             path: "/",
