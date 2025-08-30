600680052231c12146ebc249e48c6f1e0198abda
Swedka121 <ivan.hryshchuk2011@gmail.com>
2025-08-30 12:15:34 +0300

    fix tests

--- src/widget/__tests__/addAnimalcard.spec.ts
+++ src/widget/__tests__/addAnimalcard.spec.ts
@@ -1,14 +1,14 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import AddAnimalCard from "@/widget/addAnimalCard.vue"
-describe("addAnimalCard compoennt test", () => {
-    const wrapper = mount(AddAnimalCard)
+import AddAnimalCard from '@/widget/animal_board/addAnimalCard.vue'
+describe('addAnimalCard compoennt test', () => {
+  const wrapper = mount(AddAnimalCard)
 
-    it('component emit "add" event on click', async() => {
-      const buttons  = wrapper.findAll('button');
+  it('component emit "add" event on click', async () => {
+    const buttons = wrapper.findAll('button')
 
-      await buttons[0].trigger('click')
+    await buttons[0].trigger('click')
 
-      expect(wrapper.emitted("add")).toBeTruthy()
-    })
+    expect(wrapper.emitted('add')).toBeTruthy()
+  })
 })
