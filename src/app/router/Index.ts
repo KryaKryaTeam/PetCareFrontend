6aa4368e5e89c30d5cf314f5c26c8748fd295ea2
Andrii Kovalenko <andriyworkkovalenko@gmail.com>
2025-09-03 19:59:32 +0300

    add base

--- src/app/router/Index.ts
+++ src/app/router/Index.ts
@@ -11,6 +11,7 @@ import MainToListing from '@/pages/redirects/MainToListing.vue'
 import ListingLayout from '@/pages/layouts/ListingLayout.vue'
 import AuthLayout from '@/pages/layouts/AuthLayout.vue'
 import DashboardLayout from '@/pages/layouts/DashboardLayout.vue'
+import CreatePage from '@/pages/CreatePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -74,14 +75,19 @@ const router = createRouter({
       ],
     },
     {
-      path: '/app/board',
+      path: '/app/',
       name: 'dashboard',
       component: DashboardLayout,
       children: [
         {
-          path: '',
+          path: 'board',
           name: 'board',
           component: Board,
+        },
+        {
+          path: 'create',
+          name: 'create',
+          component: CreatePage,
         },
       ],
     },
